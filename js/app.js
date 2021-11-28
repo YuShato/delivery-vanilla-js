520cd8bd75cd994a102aa662e48f4c18aca553b1
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-11-28 14:03:54 +0300

    feat: add delivery functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 import Tabs from "./components/tabs.js";
+import Delivery from './components/delivery.js';
 
 export default class App {
     init() {
         const tabs = new Tabs(`.js_tabs-block`, `.js_tab`);
         tabs.init();
+
+        const delivery = new Delivery(`10:00`, `19:00`, `00:20`);
+        delivery.init();
     }
 }
--- js/constans.js
+++ js/constans.js
@@ -8,3 +8,8 @@ export const KeyCode = {
   KEY_0: 48,
   KEY_9: 57
 };
+
+export const MS_IN_MINUTE = 60000;
+export const MINUTES_IN_HOUR = 60;
+export const SECONDS_IN_HOUR = 3600;
+export  const MS_IN_SECOND = 1000;
